22b14aa831d1d819b8b9597e2b07116c284d2c0e
yogender-316 <yogendergautam316@gmail.com>
2025-12-28 17:06:48 +0530

    Day 1 finished

---
+++ week1/day1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("wertyu")
+
+
+
+	var ab = 23;
+
+	fmt.Println(ab);
+
+	fgh:="24"
+	fmt.Println(fgh)
+
+	const edf = 34
+
+	fmt.Println(edf+11)
+
+		
+}
